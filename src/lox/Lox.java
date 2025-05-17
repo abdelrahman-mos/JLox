05d1c58287ca460e19e135b962d759a727433c71
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2025-05-17 16:59:19 +0300

    Adding some scanning stuff

---
+++ src/lox/Lox.java
@@ -0,0 +1,62 @@
+package lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [input-file]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String Path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(Path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine(); // read user input on demand
+            if (line == null) break; // ctrl-D
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens(); // scan source code for tokens
+
+        for (Token token : tokens) {
+            System.out.println(token); // print tokens
+        }
+    }
+
+    // Error Handling
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+                "[Line " + line + "] Error" + where + ": " + message
+        );
+        hadError = true;
+    }
+}
